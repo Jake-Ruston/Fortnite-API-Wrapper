bda1267971a2bfffc7830b672d951d94b17cb2c7
Maverick0104 <57001106+Maverick0104@users.noreply.github.com>
2019-10-25 17:03:02 +0530

    Added a few more api uses

--- test/index.js
+++ test/index.js
@@ -8,6 +8,16 @@ const fortnite = new Client(require('./config').Token);
   const store = await fortnite.store();
 
   const challenges = await fortnite.challenges();
+  
+  const cosmetics = await fortnite.cosmetics();
+  
+  const items = await fortnite.items();
+  
+  const skins = await fortnite.skins();
+  
+  const blings = await fortnite.bblings();
+  
+  const user = await fortnite.user('&c&c', %user, %platform)
 
   console.log(user, store, challenges);
 })();
